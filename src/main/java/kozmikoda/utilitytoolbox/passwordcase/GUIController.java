30daaf7ae3003f8e276fff1cc7d8a68bfe1424b3
Criston Levato <83115432+EpicCMoment@users.noreply.github.com>
2022-12-18 00:12:44 +0300

    drag the window around

--- src/main/java/kozmikoda/utilitytoolbox/passwordcase/GUIController.java
+++ src/main/java/kozmikoda/utilitytoolbox/passwordcase/GUIController.java
@@ -1,15 +1,40 @@
 package kozmikoda.utilitytoolbox.passwordcase;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class GUIController {
+
+    double offsetX, offsetY;
 
     @FXML
     private Scene mainLayout;
 
     @FXML
+    private Pane titlebar;
+
+    @FXML
     private Stage window;
+
+
+    // bu titlebar ın onMousePressed eventi
+    @FXML
+    void dragWindow(MouseEvent event) {
+
+                window.setX(event.getScreenX() - offsetX);
+                window.setY(event.getScreenY() - offsetY);
+    }
+
+    // bu titlebar ın onMouseDragged eventi
+    @FXML
+    void setWindowOffset(MouseEvent event) {
+                offsetX = event.getSceneX();
+                offsetY = event.getSceneY();
+
+    }
 
 }
