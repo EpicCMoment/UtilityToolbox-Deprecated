fc31a114e9c53d7d1bb3a2cfaa28778f25e63edc
Criston Levato <83115432+EpicCMoment@users.noreply.github.com>
2022-12-17 23:09:25 +0300

    some tests

--- src/main/java/kozmikoda/utilitytoolbox/passwordcase/GUIController.java
+++ src/main/java/kozmikoda/utilitytoolbox/passwordcase/GUIController.java
@@ -2,14 +2,32 @@ package kozmikoda.utilitytoolbox.passwordcase;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class GUIController {
 
+    double windowX, windowY;
     @FXML
-    private Scene mainLayout;
+    private AnchorPane mainBackground;
+
+    @FXML
+    private Scene mainScene;
 
     @FXML
     private Stage window;
+
+    @FXML
+    void dragWindow(MouseEvent event) {
+        windowX = window.getX() - event.getScreenX();
+        windowY = window.getY() - event.getScreenY();
+    }
+
+    @FXML
+    void setWindowLocation(MouseEvent event) {
+        window.setX(event.getScreenX() + windowX);
+        window.setY(event.getScreenY() + windowY);
+    }
 
 }
--- src/main/java/kozmikoda/utilitytoolbox/passwordcase/GUIRunner.java
+++ src/main/java/kozmikoda/utilitytoolbox/passwordcase/GUIRunner.java
@@ -7,19 +7,26 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import kozmikoda.utilitytoolbox.cvf.HelloApplication;
 
 public class GUIRunner extends Application{
 
 
-    public void start(Stage window) throws Exception{
+    public void start(Stage s) throws Exception{
 
-        FXMLLoader mainLayout = new FXMLLoader(getClass().getResource("gui.fxml"));
-        Scene scene = new Scene(mainLayout.load(), 350, 500);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("gui.fxml"));
 
+        Stage mainWindow = loader.load();
+
+
+
+        Scene scene = mainWindow.getScene();
         scene.setFill(Color.TRANSPARENT);
-        window.initStyle(StageStyle.TRANSPARENT);
-        window.setScene(scene);
-        window.show();
+
+        mainWindow.initStyle(StageStyle.TRANSPARENT);
+        mainWindow.setTitle("Cool Voice Finder");
+        mainWindow.setScene(scene);
+        mainWindow.show();
 
     }
 
