e7089450722d12a84f55a15a31878e99cc036039
Criston Levato <83115432+EpicCMoment@users.noreply.github.com>
2022-12-18 16:47:36 +0300

    this is the working save

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,12 +9,12 @@ module kozmikoda.utilitytoolbox {
     opens kozmikoda.utilitytoolbox to javafx.fxml;
     opens kozmikoda.utilitytoolbox.passwordcase to javafx.fxml;
     opens kozmikoda.utilitytoolbox.mp3player to javafx.fxml;
-    opens kozmikoda.utilitytoolbox.cvf.cool_voice_finder_gui to javafx.fxml;
+    opens kozmikoda.utilitytoolbox.soundAnalyzer to javafx.fxml;
     opens kozmikoda.utilitytoolbox.mainapp to javafx.fxml;
 
     exports kozmikoda.utilitytoolbox;
     exports kozmikoda.utilitytoolbox.mp3player;
     exports kozmikoda.utilitytoolbox.passwordcase;
-    exports kozmikoda.utilitytoolbox.cvf.cool_voice_finder_gui;
+    exports kozmikoda.utilitytoolbox.soundAnalyzer;
     exports kozmikoda.utilitytoolbox.mainapp;
 }
