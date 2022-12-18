36e39cde5c45f25a4aa7b1a58420e184e5e9d832
Criston Levato <83115432+EpicCMoment@users.noreply.github.com>
2022-12-18 15:55:01 +0300

    delete unnecessary files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,13 +6,11 @@ module kozmikoda.utilitytoolbox {
     requires com.jfoenix;
     requires java.desktop;
 
-    opens kozmikoda.utilitytoolbox to javafx.fxml;
     opens kozmikoda.utilitytoolbox.passwordcase to javafx.fxml;
     opens kozmikoda.utilitytoolbox.mp3player to javafx.fxml;
     opens kozmikoda.utilitytoolbox.soundAnalyzer to javafx.fxml;
     opens kozmikoda.utilitytoolbox.mainapp to javafx.fxml;
 
-    exports kozmikoda.utilitytoolbox;
     exports kozmikoda.utilitytoolbox.mp3player;
     exports kozmikoda.utilitytoolbox.passwordcase;
     exports kozmikoda.utilitytoolbox.soundAnalyzer;
